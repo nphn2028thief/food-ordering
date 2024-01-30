8e641a24477e13bf237efcd6a1874fadcd90fc15
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-01-30 16:44:35 +0700

    Implement responsive navbar on mobile part 1

--- src/layouts/components/Navbar/index.tsx
+++ src/layouts/components/Navbar/index.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
 
 import { CPath } from "@/constanst/path";
 import Button from "../../../components/common/Button";
 
-const Header = () => {
+const Navbar = () => {
+  const [showSidebarMobile, setShowSidebarMobile] = useState<boolean>(false);
+
   return (
     <header className="flex justify-between items-center p-4">
       <Link
@@ -13,7 +19,7 @@ const Header = () => {
         st pizza
       </Link>
 
-      <nav className="flex text-gray-500 font-semibold">
+      <nav className="hidden md:flex text-gray-500 font-semibold">
         <Link href={""} className="px-8 py-2 capitalize">
           home
         </Link>
@@ -31,8 +37,22 @@ const Header = () => {
           Login
         </Button>
       </nav>
+
+      <button
+        className="flex md:hidden p-2 pl-3 pr-4 -mr-4"
+        onClick={() => setShowSidebarMobile(true)}
+      >
+        <BiMenuAltRight size={28} />
+      </button>
+
+      {showSidebarMobile ? (
+        <div
+          className="fixed inset-0 bg-black/15"
+          onClick={() => setShowSidebarMobile(false)}
+        ></div>
+      ) : null}
     </header>
   );
 };
 
-export default Header;
+export default Navbar;
