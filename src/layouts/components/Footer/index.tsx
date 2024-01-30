0ef14440521e4d82bcef2be25677e8fc92285190
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-01-30 13:45:11 +0700

    Implement ui home page

---
+++ src/layouts/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { FaRegCopyright } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center gap-2 p-8 border-t">
+      <FaRegCopyright />
+      2024 All rights reserved
+    </div>
+  );
+};
+
+export default Footer;
